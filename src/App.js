42fb4e20e09238754fd2f3d83d6b7c4f56193b40
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-09-20 11:14:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import About from './components/About';
-import {useState } from 'react';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
- import { BrowserRouter as Main,Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Main, Route, Routes } from 'react-router-dom';
 
 
 
@@ -11,52 +11,54 @@ function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-        msg: message,
-        type: type
+      msg: message,
+      type: type
     })
     setTimeout(() => {
       setAlert(null);
-    },1600);
-} 
-  const toggleMode =()=>{
-    if(mode === 'light'){
+    }, 1600);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='#042743';
-      showAlert("Dark mode is Enabled","success");
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode is Enabled", "success");
     }
-    else{
+    else {
       setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode is Enabled","success");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is Enabled", "success");
     }
+
   }
 
   return (
     <>
-    <Main>
-      <Navbar title="TextUtils" mode={mode} aboutText="About" toggleMode={toggleMode} />
-      <div className="container">
-      <Alert alert={alert} />
-      </div>
-      
-      
-      <div className="container my-5">
-        <Routes>
-          <Route exact path='/' element={<TextForm heading="TextUtils - Word Counter, Character Counter" showAlert={showAlert} mode={mode} />} >
-          </Route> 
-          <Route exact path='/about' element={<About  mode={mode} />} >
-          </Route>
-        </Routes>
-        
-        
-      </div>
-      
-    </Main>
+      <Main>
+        <Navbar title="TextUtils" mode={mode} aboutText="About" toggleMode={toggleMode} />
+        <div className="container">
+          <Alert alert={alert} />
+        </div>
+
+
+        <div className="container my-5">
+          <Routes>
+            <Route exact path='/' element={<TextForm heading="TextUtils - Word Counter, Character Counter" showAlert={showAlert} mode={mode} />} >
+            </Route>
+            <Route exact path='/about' element={<About mode={mode} />} >
+            </Route>
+          </Routes>
+
+
+        </div>
+
+      </Main>
     </>
-    
+
   );
 }
+
 
 export default App;
